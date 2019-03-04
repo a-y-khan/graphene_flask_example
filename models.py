5c30f56ed81d7db7cecc02692e1d7e9629f860d2
Ayla Khan <ayla.khan@gmail.com>
2019-03-04 00:04:46 -0700

    Add doc strings.

--- models.py
+++ models.py
@@ -8,21 +8,23 @@ class House(Base):
 	__tablename__ = 'house'
 	id = Column(Integer, primary_key=True)
 	name = Column(String)
-	location = Column(String)
-	founder = Column(String)
-	colors = Column(String)
-	crest = Column(String)
-	ghost = Column(String)
+	location = Column(String, doc="Location on Hogwarts campus.")
+	founder = Column(String, doc="Hogwarts house founder.")
+	colors = Column(String, doc="Each Hogwarts house has two colors.")
+	crest = Column(String, doc="Hogwarts house crest.")
+	ghost = Column(String, doc="Each Hogwarts house has a resident ghost.")
 
+
+## TODO: create Wand table?
 
 class Student(Base):
 	__tablename__ = 'student'
 	id = Column(Integer, primary_key=True)
-	name = Column(String)
-	wand_wood = Column(String)
-	wand_core = Column(String)
-	wand_length = Column(Float)
-	wand_length_unit = Column(String)
+	name = Column(String, doc="Hogwarts student name. Typically first name, surname.")
+	wand_wood = Column(String, doc="Hogwarts student's wand material.")
+	wand_core = Column(String, doc="Hogwarts student's wand core.")
+	wand_length = Column(Float, doc="Length of student's wand.")
+	wand_length_unit = Column(String, doc="Measurement unit used for wand length.")
 	house_id = Column(Integer, ForeignKey('house.id'))
 	house = relationship(
 		House,
@@ -33,8 +35,8 @@ class Student(Base):
 class Pet(Base):
 	__tablename__ = 'pet'
 	id = Column(Integer, primary_key=True)
-	name = Column(String)
-	species = Column(String)
+	name = Column(String, doc="Pet name.")
+	species = Column(String, doc="Pet species.")
 	student_id = Column(Integer, ForeignKey('student.id'))
 	student = relationship(
 		Student,
