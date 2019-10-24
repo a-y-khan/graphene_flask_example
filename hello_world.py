8162dd27b1513ca46c9eb8a936b8b9f9c6110cfd
Ayla Khan <ayla.khan@gmail.com>
2019-10-23 22:17:38 -0600

    Add hello world example.

---
+++ hello_world.py
@@ -0,0 +1,32 @@
+import graphene as gp
+from gql import gql, Client
+
+class Query(gp.ObjectType):
+    hello = gp.String(argument=gp.String(default_value='world'))
+
+    # this is where the magic happens!
+    # pattern is always resolve_<field>
+    def resolve_hello(self, info, argument):
+        # info:
+        # ResolveInfo class: https://github.com/graphql-python/graphql-core
+        return 'hello ' + argument
+
+schema = gp.Schema(query=Query)
+
+if '__main__' == __name__:
+    # GraphQL's default serialization format is JSON!
+    query = """
+    {
+        hello
+    }
+    """
+    query_with_argument = """
+    {
+        hello(argument: "universe!")
+    }
+    """
+    # result is OrderedDict
+    result = schema.execute(query)
+    print(result.data['hello'])
+    result = schema.execute(query_with_argument)
+    print(result.data['hello'])
